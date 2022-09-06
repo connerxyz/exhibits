5e641c460aec825b284aecbcf88a19bfee53638a
Conner Cowling <conner.cowling@thomsonreuters.com>
2022-09-06 13:03:53 -0500

    Build now creates CNAME based on configuration.

--- cxyz/freezer/__main__.py
+++ cxyz/freezer/__main__.py
@@ -7,8 +7,10 @@ log = logging.getLogger()
 
 freezer = Freezer(app)
 
-app.config['FREEZER_DESTINATION'] = "../docs/"
-app.config['FREEZER_BASE_URL'] = "https://exhibits.conner.xyz"
+BUILD_DIR = "docs/"
+BASE_DOMAIN = "exhibits.conner.xyz"
+app.config['FREEZER_BASE_URL'] = f"https://{BASE_DOMAIN}"
+app.config['FREEZER_DESTINATION'] = f"../{BUILD_DIR}"
 app.config['FREEZER_IGNORE_MIMETYPE_WARNINGS'] = True
 app.config['FREEZER_STATIC_IGNORE'] = [
     '**/_/*', # TODO: doesn't work
@@ -23,4 +25,7 @@ if __name__ == '__main__':
     log.info("Compiling site to static build using Flask Freezer.")
     log.info(f"Using base URL {app.config['FREEZER_BASE_URL']}.")
     freezer.freeze()
+    log.info(f"Adding CNAME for GitHub Pages: {BASE_DOMAIN}")
+    with open(f"{BUILD_DIR}/CNAME", "w") as f:
+        f.write(BASE_DOMAIN)
     log.info(f"New site build written to {app.config['FREEZER_DESTINATION']}")
